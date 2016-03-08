000510bf6c7cacc2e674ba5ce2601d99c4279f19
Anish Hallan <anishhallan@gmai.com>
2016-03-08 16:25:55 +0000

    Initial Commit

---
+++ Instamash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instamash
+//
+//  Created by anish home on 08/03/2016.
+//  Copyright © 2016 anish home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
